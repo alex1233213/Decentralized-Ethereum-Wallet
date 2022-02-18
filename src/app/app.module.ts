06ff5ff534683459a7844b7190e14530d7ddf599
Alex <alexanderbulgari@gmail.com>
2022-02-18 14:34:10 +0000

    refactor app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,48 +6,22 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   NbThemeModule,
-  NbLayoutModule,
-  NbCardModule,
-  NbButtonModule,
-  NbInputModule,
-  NbStepperModule, NbCheckboxModule, NbAlertModule, NbSpinnerModule, NbSidebarModule
+  NbSidebarModule
 } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
-import { CreateWalletComponent } from './components/create-wallet/create-wallet.component';
-import { CreatePasswordComponent } from './components/create-wallet/create-password/create-password.component';
-import { ReactiveFormsModule } from "@angular/forms";
-import { WalletConfirmationComponent } from './components/create-wallet/wallet-confirmation/wallet-confirmation.component';
-import { DisplayMnemonicComponent } from './components/create-wallet/display-mnemonic/display-mnemonic.component';
-import { VerifyMnemonicComponent } from './components/create-wallet/verify-mnemonic/verify-mnemonic.component';
-import { AccessWalletComponent } from './components/access-wallet/access-wallet.component';
 import { LayoutModule } from "./layout/layout.module";
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CreateWalletComponent,
-    CreatePasswordComponent,
-    WalletConfirmationComponent,
-    DisplayMnemonicComponent,
-    VerifyMnemonicComponent,
-    AccessWalletComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'default' }),
-    NbLayoutModule,
     NbEvaIconsModule,
-    NbInputModule,
-    ReactiveFormsModule,
-    NbCardModule,
-    NbButtonModule,
-    NbStepperModule,
-    NbCheckboxModule,
-    NbAlertModule,
-    NbSpinnerModule,
     NbSidebarModule.forRoot(),
     LayoutModule
   ],
