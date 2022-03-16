149ae65abd23caf526e9af27466779754c3486d8
Alex <alexanderbulgari@gmail.com>
2022-03-16 16:58:55 +0000

    update gas 3 seconds

--- src/app/wallet/dialogs/confirm-tx-dialog/confirm-tx-dialog.component.ts
+++ src/app/wallet/dialogs/confirm-tx-dialog/confirm-tx-dialog.component.ts
@@ -1,40 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Token } from "../../../shared/utils/Token";
 import { TransactionService } from "../../../services/transaction/transaction.service";
 import { Wallet } from "ethers";
-import {tokenAddresses} from "../../../shared/utils/token-addresses";
 import { NbDialogRef } from "@nebular/theme";
+import Timeout = NodeJS.Timeout;
 
 @Component({
   selector: 'app-confirm-tx-dialog',
   templateUrl: './confirm-tx-dialog.component.html',
   styleUrls: ['./confirm-tx-dialog.component.css']
 })
-export class ConfirmTxDialogComponent implements OnInit {
+export class ConfirmTxDialogComponent implements OnInit, OnDestroy {
 
   send_token: Token;
   send_amount: number;
   recipient_address: string;
   wallet: Wallet;
   gasFee: number;
+  interval_id: Timeout;
 
   constructor(private txService: TransactionService,
               protected dialogRef: NbDialogRef<ConfirmTxDialogComponent>) { }
 
   ngOnInit(): void {
-    // setInterval( async () => {
-    //   this.gasFee = await this.txService.estimateGasFee(this.wallet);
-    //   console.log('this.gasFee: ' + this.gasFee);
-    // }, 3000);
+    //update the gas fee every 3 seconds
+    this.interval_id = setInterval( async () => {
+      await this.estimateTransactionFee();
+      console.log('this.gasFee: ' + this.gasFee);
+    }, 3000);
+  }
 
 
-    this.estimateTransactionFee();
+  ngOnDestroy() {
+    clearInterval(this.interval_id);
   }
 
 
   async estimateTransactionFee() {
     this.gasFee = await this.txService.estimateGasFee(this.send_token, this.wallet);
-    console.log('this.gasFee: ' + this.gasFee);
+    // console.log('this.gasFee: ' + this.gasFee);
   }
 
 
