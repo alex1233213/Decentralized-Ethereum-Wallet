f1d0c88cf3e1e739c9adea44c076b648eb2a7f30
Alex <alexanderbulgari@gmail.com>
2022-03-24 20:54:31 +0000

    loading transactions spinner

--- src/app/wallet/tx-history/tx-history.component.ts
+++ src/app/wallet/tx-history/tx-history.component.ts
@@ -12,16 +12,19 @@ export class TxHistoryComponent implements OnInit {
 
   tx_history: any;
   wallet: Wallet;
+  loading_data: boolean;
 
   constructor(private txHistoryService: TxHistoryService,
               private walletService: WalletService) { }
 
   ngOnInit(): void {
     this.walletService.getWallet().subscribe( (wallet: Wallet) =>{
+      this.loading_data = true;
       this.wallet = wallet;
       this.txHistoryService.getTxHistory(this.wallet).then( history => {
         this.tx_history = history;
         console.log(this.tx_history);
+        this.loading_data = false;
       });
     });
   }
